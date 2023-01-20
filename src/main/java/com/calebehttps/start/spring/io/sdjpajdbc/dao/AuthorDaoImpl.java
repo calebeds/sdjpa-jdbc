2653209db78980582f2a36eb460b55cf7f74edb3
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-01-20 00:02:52 -0300

    create row mapper

--- src/main/java/com/calebehttps/start/spring/io/sdjpajdbc/dao/AuthorDaoImpl.java
+++ src/main/java/com/calebehttps/start/spring/io/sdjpajdbc/dao/AuthorDaoImpl.java
@@ -1,6 +1,7 @@
 package com.calebehttps.start.spring.io.sdjpajdbc.dao;
 
 import com.calebehttps.start.spring.io.sdjpajdbc.domain.Author;
+import org.springframework.jdbc.core.RowMapper;
 
 public class AuthorDaoImpl implements AuthorDao {
     @Override
@@ -26,5 +27,9 @@ public class AuthorDaoImpl implements AuthorDao {
     @Override
     public void deleteAuthorById(Long id) {
 
+    }
+
+    private RowMapper<Author> getRowMapper() {
+        return new AuthorMapper();
     }
 }
